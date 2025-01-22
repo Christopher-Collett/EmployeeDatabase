efe84698a173e6690282b3667399771ace311e5c
Christopher Collett <christopheracollett@gmail.com>
2025-01-21 22:09:53 -0600

    Added further exploration of double points and realloc to dynamic.c

--- dynamic.c
+++ dynamic.c
@@ -4,6 +4,12 @@
 
 #define MAX_EMPLOYEES 1000
 
+typedef enum
+{
+    SUCCESS,
+    FAILURE
+} ReturnCode;
+
 struct Employee
 {
     int id;
@@ -11,27 +17,47 @@ struct Employee
     bool staff;
 };
 
-void intialize_employee(struct Employee *e)
+void intialize_employee(struct Employee *e, int id)
 {
-    e->id = 0;
+    e->id = id;
     e->income = 0;
     e->staff = false;
 }
 
+ReturnCode add_employee(struct Employee **employees)
+{
+    static int numberEmployees = 0;
+    numberEmployees++;
+    struct Employee *temp = realloc(*employees, numberEmployees * sizeof(struct Employee));
+    if (temp == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return FAILURE;
+    }
+    *employees = temp; // using a temp to avoid freeing employees if realloc fails
+    intialize_employee(&((*employees)[numberEmployees - 1]), numberEmployees);
+    return SUCCESS;
+}
+
 int main()
 {
-    int n = 4;
-    struct Employee *employees = malloc(sizeof(struct Employee) * n);
+    struct Employee *employees = malloc(sizeof(struct Employee));
     if (employees == NULL)
     {
         printf("Memory allocation failed\n"); // either free memory and try again or exit
         return 1;
     }
 
-    for (int i = 0; i < n; i++)
-        intialize_employee(&employees[i]);
+    for (int i = 0; i < 4; i++)
+    {
+        if (add_employee(&employees) == FAILURE)
+        {
+            printf("Failed to add employee\n");
+            return 1;
+        }
+    }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 4; i++)
         printf("Employee %d has an income of %d\n", employees[i].id, employees[i].income);
 
     free(employees);
